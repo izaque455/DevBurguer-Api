cf61bf9de27e77e0379170153dc155fe9195cf94
izaque455 <pereiraizaque407@gmail.com>
2026-06-21 14:44:04 -0300

    Ajuste na model de Produtos pt 1

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -6,7 +6,6 @@ class Product extends Model {
       {
         name: Sequelize.STRING,
         price: Sequelize.INTEGER,
-        category: Sequelize.STRING,
         path: Sequelize.STRING,
         url: {
           type: Sequelize.VIRTUAL,
@@ -20,6 +19,13 @@ class Product extends Model {
         tableName: 'products',
       },
     );
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Category, {
+      foreignKey: 'category_id',
+      as: 'category',
+    });
   }
 }
 
