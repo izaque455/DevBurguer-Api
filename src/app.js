de7eb465cb84234f61cec534149b9503de848896
izaque455 <pereiraizaque407@gmail.com>
2026-06-26 14:39:17 -0300

    CORS Implementation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
+import cors from 'cors';
 import express from 'express';
-import routes from './routes.js';
 import fileRoutesConfig from './config/fileRoutes.cjs';
+import routes from './routes.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/product-file', fileRoutesConfig);
 app.use('/category-file', fileRoutesConfig);
+app.use(cors());
 
 app.use(routes);
 export default app;
